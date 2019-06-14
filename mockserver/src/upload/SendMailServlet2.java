a5f656a310e9c94ff1270e0b0454689d7b92d60a
dingyuanzheng <dingyuanzheng@gaosiedu.com>
2019-06-14 16:00:45 +0800

    feat |> 删除无用代码

--- mockserver/src/upload/SendMailServlet2.java
+++ mockserver/src/upload/SendMailServlet2.java
@@ -1,30 +1,18 @@
 package upload;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import utils.Analyze;
 import utils.CommentUtil;
 import utils.MailUtils;
 import utils.StringUtil;
-
-import com.google.gson.reflect.TypeToken;
 
 public class SendMailServlet2 extends HttpServlet {
 	
